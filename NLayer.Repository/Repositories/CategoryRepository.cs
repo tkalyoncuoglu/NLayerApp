5bb8401bcb7b5a66009150d2c6fd207f4dcdcca1
Berk <136861042+berkcavdar@users.noreply.github.com>
2023-09-06 16:45:45 +0300

    Icategoryrepository interaface and its class created. Categories controller added for category api endpoints.

---
+++ NLayer.Repository/Repositories/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }   
+
+        public async Task<Category> GetSingleCategoryByIdWithProductAsync(int categoryId)
+        {
+            //First Not returns an exception error 
+            //Single returns an exception error for more duplicated keys
+            return await _context.Categories.Include(x => x.Products).Where(x => x.Id == categoryId).SingleOrDefaultAsync();
+        }
+    }
+}
