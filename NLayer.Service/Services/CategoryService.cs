7b27f201106daecf9ddb5ed91771c3e39bf2dc09
Berk <136861042+berkcavdar@users.noreply.github.com>
2023-09-06 18:15:33 +0300

    Category controller modified with task and injected to DI . Created new dto for categorywith product , it's service interface and class . At final added mapping route and scoped to category service and repository.

---
+++ NLayer.Service/Services/CategoryService.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayer.Core.UnitOfWorks;
+
+namespace NLayer.Service.Services
+{
+    public class CategoryService : Service<Category>, ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IGenericRepository<Category> genericRepository, IUnitOfWork unitOfWork, ICategoryRepository categoryRepository, IMapper mapper) : base(genericRepository, unitOfWork)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<CustomResponseDTO<CategoryWithProductsDTO>> GetSingleCategoryByIdWithProducts(int categoryId)
+        {
+            var category = await _categoryRepository.GetSingleCategoryByIdWithProductAsync(categoryId);
+            var categoryDto = _mapper.Map<CategoryWithProductsDTO>(category);
+
+            return CustomResponseDTO<CategoryWithProductsDTO>.Success(200,categoryDto);
+        }
+    }
+}
