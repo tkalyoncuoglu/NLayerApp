ddad0181da4ab954c65443116387ffb547708a25
Berk <136861042+berkcavdar@users.noreply.github.com>
2023-09-06 14:09:41 +0300

    Created CustomResponse DTO file for succes and fail responses and created with static fathory methods for easy to use by clients.

---
+++ NLayer.Core/DTOs/CustomResponseDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.DTOs
+{
+    public class CustomResponseDTO<T>
+    {
+        public T Data { get; set; }
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+        public List<String> Errors { get; set; }
+
+        // Static Factory Method
+        public static CustomResponseDTO<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode, Data = data};
+        }
+        // Static Factory Method
+        public static CustomResponseDTO<T> Success(int statusCode)
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode };
+        }
+        // Static Factory Method
+        public static CustomResponseDTO<T> Fail(int statusCode, List<String> errors) 
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode, Errors = errors};
+        }
+        // Static Factory Method
+        public static CustomResponseDTO<T> Fail(int statusCode, string error)
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode, Errors = new List<string> { error } };
+    }
+}
